cd35c7a9de2e16a6be789c0d55de80b38391587c
LORYMAX <lorymax4@gmail.com>
2021-03-26 11:06:38 +0100

    Creazione interfacce di base dei controller per le operazioni CRUD

---
+++ src/main/java/it/plansoft/classmanager/controller/ICrudController.java
@@ -0,0 +1,19 @@
+package it.plansoft.classmanager.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICrudController<MODEL, ID> extends IController {
+
+	public List<MODEL> getAll();
+	
+	public Optional<MODEL> getById(ID id);
+	
+	public MODEL update(MODEL model, ID id);
+	
+	public MODEL add(MODEL model);
+	
+	public void delete(MODEL model);
+	
+	public void deleteById(ID id);
+}
