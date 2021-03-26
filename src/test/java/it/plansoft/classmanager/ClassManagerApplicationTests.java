b35030805e5862edb873c5585b23001439c3a321
Leonardo Morganti <leonardo.morganti00@gmail.com>
2021-03-26 10:21:26 +0100

    Inizializzazione progetto

---
+++ src/test/java/it/plansoft/classmanager/ClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.plansoft.classmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
