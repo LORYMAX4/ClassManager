a41451bc736b956a56b6c045a2ad62b4caa955e3
thejoin95 <miki.lombi@gmail.com>
2021-04-21 14:51:40 +0200

    add main buttons in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import { SimpleGrid, Box } from "@chakra-ui/react"
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,54 +14,35 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Benvenuto in ClassManager
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Scegli l'azione che vuoi eseguire
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <SimpleGrid columns={2} spacing={10}>
+          <Box className={styles.card}>
+            <Link href="/classrooms">
+              <a>
+                <h3>Classi &rarr;</h3>
+                <p>Visualizza un elenco di classi</p>
+              </a>
+            </Link>
+          </Box>
+          <Box className={styles.card}>
+            <Link href="/students">
+              <a>
+                <h3>Studenti &rarr;</h3>
+                <p>Visualizza gli studenti</p>
+              </a>
+              </Link>
+          </Box>
+        </SimpleGrid>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Powered by Plansoft
       </footer>
     </div>
   )
