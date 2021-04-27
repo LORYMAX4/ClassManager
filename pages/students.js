a352592ff4f3644a94d42068036b21adb031c86f
Antonio Caia <antonio.caia96@gmail.com>
2021-04-27 12:06:02 +0200

    Fix: students and classrooms are shown correctly

--- pages/students.js
+++ pages/students.js
@@ -4,55 +4,55 @@ import Link from "next/link";
 import { FaPencilAlt, FaSearch, FaTimes, FaUser } from 'react-icons/fa';
 
 function Students(props) {
-  let students = [];
+	let students = [];
 
-  for (let i = 0; i < props.data.length; i++) {
-    let student = props.data[i];
-    students.push((
-      <Box className="card">
-        <SimpleGrid columns={2} spacing={5}>
-          <Box>
-            <IconButton icon={<FaUser />} />
-            <h3 style={{marginLeft: '10px', display: 'inline-block'}}>{student.name} {student.surname}</h3>
-            <p><small>{student.taxCode}</small></p>
-          </Box>
-          <Box style={{textAlign: "right"}}>
-            <Link href={"/students/" + student.id}>
-              <IconButton bg="blue.400" color="white" marginRight={3} aria-label="Get" icon={<FaSearch />} />
-            </Link>
-            <IconButton bg="green.400" color="white" marginRight={3} aria-label="Update" icon={<FaPencilAlt />} />
-            <IconButton bg="red.400" color="white" aria-label="Delete" icon={<FaTimes />} />
-          </Box>
-        </SimpleGrid>
-      </Box>
-    ));
-  }
+	for (let i = 0; i < props.data.length; i++) {
+		let student = props.data[i];
+		students.push((
+			<Box className="card">
+				<SimpleGrid columns={2} spacing={5}>
+					<Box>
+						<IconButton icon={<FaUser />} />
+						<h3 style={{ marginLeft: '10px', display: 'inline-block' }}>{student.name} {student.surname}</h3>
+						<p><small>{student.taxCode}</small></p>
+					</Box>
+					<Box style={{ textAlign: "right" }}>
+						<Link href={"/students/" + student.id}>
+							<IconButton bg="blue.400" color="white" marginRight={3} aria-label="Get" icon={<FaSearch />} />
+						</Link>
+						<IconButton bg="green.400" color="white" marginRight={3} aria-label="Update" icon={<FaPencilAlt />} />
+						<IconButton bg="red.400" color="white" aria-label="Delete" icon={<FaTimes />} />
+					</Box>
+				</SimpleGrid>
+			</Box>
+		));
+	}
 
-  return (
-      <Container maxW="2xl" centerContent>
-        <Text fontSize="2em" align="center">Students</Text>
-        <SimpleGrid columns={1} spacing={1}>
-          {students}
-        </SimpleGrid>
-      </Container>
-    );
-  }
-  
+	return (
+		<Container maxW="2xl" centerContent>
+			<Text fontSize="2em" align="center">Students</Text>
+			<SimpleGrid columns={1} spacing={1}>
+				{students}
+			</SimpleGrid>
+		</Container>
+	);
+}
+
 export async function getStaticProps(context) {
-  const res = await fetch(`http://localhost:8080/classmanager/classrooms/1`)
-  let data = await res.json()
+	const res = await fetch(`http://localhost:8080/classmanager/students/`)
+	let data = await res.json()
 
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
+	if (!data) {
+		return {
+			notFound: true,
+		}
+	}
 
-  data = data.students || data;
+	data = data.students || data;
 
-  return {
-    props: { data }, // will be passed to the page component as props
-  }
+	return {
+		props: { data }, // will be passed to the page component as props
+	}
 }
 
 export default Students;
--- src/main/java/it/plansoft/classmanager/model/Classroom.java
+++ src/main/java/it/plansoft/classmanager/model/Classroom.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "classrooms")
+@Table(name = "CLASSES")
 public class Classroom extends IDModel<Long> {
 
 	@Id
