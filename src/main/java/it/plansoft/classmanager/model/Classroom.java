0b39e3e5be099ca1ff6fed9bad00461ecba64efb
Antonio Caia <antonio.caia96@gmail.com>
2021-05-05 15:55:13 +0200

    Test: add unit tests for ClassroomService

--- src/main/java/it/plansoft/classmanager/model/Classroom.java
+++ src/main/java/it/plansoft/classmanager/model/Classroom.java
@@ -57,4 +57,46 @@ public class Classroom extends IDModel<Long> {
 	public void setStudents(Set<Student> students) {
 		this.students = students;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + grade;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((students == null) ? 0 : students.hashCode());
+		return result;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		
+		Classroom other = (Classroom) obj;
+		if (grade != other.grade) return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		
+		if (students == null) {
+			if (other.students != null)
+				return false;
+		} else if (!students.equals(other.students))
+			return false;
+		
+		return true;
+	}
+
 }
---
+++ src/test/java/it/plansoft/classmanager/service/ClassroomServiceTest.java
@@ -0,0 +1,84 @@
+package it.plansoft.classmanager.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import it.plansoft.classmanager.model.Classroom;
+import it.plansoft.classmanager.repository.ClassroomRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClassroomServiceTest {
+	
+	@Mock ClassroomRepository repository;
+	@InjectMocks ClassroomService service;
+	
+	final Long id = 12L;
+	final int grade = 1;
+	final String name = "myClass";
+	
+	@Test
+	public void testFindByGradeName() {
+		Optional<Classroom> testClassroom = Optional.of(new Classroom(name, grade));
+		Optional<Classroom> expectedClassroom = Optional.of(new Classroom(name, grade));
+		when(repository.findByNameAndGrade(name, grade)).thenReturn(testClassroom);
+		
+		assertEquals(expectedClassroom, service.findByGradeName(grade, name));
+	} 
+	
+	@Test 
+	public void testFindById() {
+		Optional<Classroom> testClassroom = Optional.of(new Classroom(name, grade));
+		Optional<Classroom> expectedClassroom = Optional.of(new Classroom(name, grade));
+		when(repository.findById(id)).thenReturn(testClassroom);
+		
+		assertEquals(expectedClassroom, service.findById(id));
+	}
+	
+	@Test
+	public void testFindAll() {
+		Classroom c1 = new Classroom("class1", 1);
+		Classroom c2 = new Classroom("class2", 2);		
+		List<Classroom> expectedClasses = Arrays.asList(c1, c2);
+		when(repository.findAll()).thenReturn(Arrays.asList(c1, c2));
+		
+		List<Classroom> actualClasses = service.findAll(); 
+		
+		assertEquals(expectedClasses, actualClasses);
+	}
+	
+	@Test	
+	public void testSaveOrder() {
+		Classroom testClassroom = new Classroom(name, grade);
+		when(repository.save(testClassroom)).thenReturn(testClassroom);
+		
+		assertEquals(testClassroom, service.save(testClassroom));
+	}
+
+	@Test
+	public void testDeleteById() {
+		service.deleteById(id);
+		
+		verify(repository).deleteById(id);
+	}
+
+	@Test
+	public void testDelete() {
+		Classroom testClassroom = new Classroom(name, grade);
+		
+		service.delete(testClassroom);
+		
+		verify(repository).delete(testClassroom);
+	}
+
+}
